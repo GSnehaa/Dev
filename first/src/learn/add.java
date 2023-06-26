157dc6de4bcb2c89ad5b01ab94515a8c62a4d1e8
GSnehaa <82664055+GSnehaa@users.noreply.github.com>
2023-06-26 11:29:23 +0530

    test
    test

---
+++ first/src/learn/add.java
@@ -0,0 +1,17 @@
+package learn;
+
+public class add {
+
+	public static void main(String[] args) {
+		int a=99,b=100,c=a+b;
+		
+	//	System.out.println((a<=b)||(a>b));
+	//	System.out.println(a=b);
+	//	System.out.println(a);
+	//	System.out.println(!(a==b));
+	//	System.out.println(a!=b);
+		System.out.println("c is " + c);
+
+	}
+
+}
---
+++ first/src/learn/cntrlst.java
@@ -0,0 +1,103 @@
+package learn;
+
+public class cntrlst {
+
+	public static void main(String[] args) {
+		
+//		int result = getindex(45);
+		int i =1123;
+		reverse(i);
+//		 int u =i++;
+//		int ii= ++i;
+//		int y =--i;
+//		int h = i--;
+		
+	}
+	
+	public static int reverse(int a) {
+		int rev=0;
+		while(a > 0) {
+			int rem = a % 10;
+			rev  = rev* 10 +  rem;
+			a/=10;
+		}
+		return rev;
+	}
+	
+	public static Integer getindex(int var) {
+		Integer r = null;
+		int a [] = {12,34,45,56,6,6};
+		if(a.length > 0) {
+			for(int i=0;i< a.length;i++) {
+				if(a[i] == var) {
+					r = i;
+					break;
+				}
+			}
+		}
+		return r;
+	}
+		
+}
+
+//methods or functions 
+
+//1. with return with parameters
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		date time 
+		
+//		int a [] = {12,67,78,0,89};
+//		
+//		if(a.length > 0) {
+//			for(int i =0;i< a.length;i++) {
+//				
+//			}
+//		}
+		
+		
+//		if 
+//		else if 
+//		while 
+//		for 
+//		do while 
+//		int a =112;
+//		if(a < 13) {
+//			System.out.println(a);
+//		}else {
+//			System.out.println("----");
+//		}
+		
+//		nested if 
+//		int a = 12;
+//		int b =23;
+//		
+//		if(a  < 15 && a == 12) {
+//			if(b == 23) {
+//				
+//			}
+//		}
+		
+//for(int i=0;i< 10;i++) {
+//	
+//}
+//int a =1;
+//while (a < 10) {
+//	System.out.print(a);
+//	a++;
+//}
+
+
+//step 1:
+//a = 112 112 < 13 false else
+
+
